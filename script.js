18a42b82e05860275b750bddc8c03263a9bd67e9
Barbara Damasceno <barbaradamas.dev@gmail.com>
2023-11-14 19:28:57 -0300

    responsive mobile and tablet style

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu () {
+    document.getElementById('navbar-items').classList.toggle('active');
+    document.getElementById('bg-close-menu').classList.toggle('active');
+    document.getElementById('line1').classList.toggle('change');
+    document.getElementById('line2').classList.toggle('change');
+    document.getElementById('line3').classList.toggle('change');
+}
